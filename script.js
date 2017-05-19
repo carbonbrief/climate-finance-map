f00ffdcefc7ccba3095eeb7b1fb59e8c7ba377ce
Rosamund <rosamund.pearce@carbonbrief.org>
2017-05-19 14:29:46 +0100

    correct latlng error in geojson

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-var mymap = L.map('mapid').setView([31.505, 7], 2);
+var myMap = L.map('mapid').setView([31.505, 7], 2);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/rospearce/ciwgju4yv00cy2pmqeggx1mx8/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9zcGVhcmNlIiwiYSI6ImNpdm1sczJsZjAwOGMyeW1xNHc4ejJ0N28ifQ.4B24e0_HgfJj4sgqimETqA', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox://styles/rospearce/ciwgju4yv00cy2pmqeggx1mx8',
     accessToken: 'pk.eyJ1Ijoicm9zcGVhcmNlIiwiYSI6ImNpdm1sczJsZjAwOGMyeW1xNHc4ejJ0N28ifQ.4B24e0_HgfJj4sgqimETqA'
-}).addTo(mymap);
+}).addTo(myMap);
 
-var promise = $.getJSON("climatefinance.geojson");
-promise.then(function(data) {
-	var allProjects = L.geoJson(data, {
-		pointToLayer: function(feature, latlng) {
-			return L.circleMarker(latlng)
-		},
-		onEachFeature: onEachFeature,
-		style: style
-	});
- allProjects.addTo(mymap);
-})
+
+
+$.ajaxSetup({
+  scriptCharset: "utf-8",
+  contentType: "application/json; charset=utf-8"
+});
+//
+// $.ajax('climatefinance.geojson').done(function(data) {
+//   data = JSON.parse(data);
+//   console.log(data);
+//   L.geoJson(data, {
+//
+//   }).addTo(myMap);
+//
+// });
+
+$.ajax({
+        url: "climatefinance.geojson",
+        beforeSend: function(xhr){
+          if (xhr.overrideMimeType)
+          {
+            xhr.overrideMimeType("application/json");
+          }
+        },
+        dataType: 'json',
+        data: null,
+        success:  function(data, textStatus, request) {
+        var allProjects = L.geoJson(data, {
+					  pointToLayer: function (feature, latlng) {
+					      return L.circleMarker(latlng);
+					  },
+						onEachFeature: onEachFeature,
+						style: style
+				});
+				allProjects.addTo(myMap);
+			}
+});
 
 function style(feature) {
     return {
@@ -38,4 +64,18 @@ function onEachFeature(feature, layer) {
                 layer.on('mouseout', function() { layer.closePopup(); });
     };
 }
+
+// var dataLayer = $.getJSON("climatefinance.geojson");
+// dataLayer.then(function(data) {
+// 	var allProjects = L.geoJson(data, {
+// 	  pointToLayer: function (feature, latlng) {
+// 	      return L.circleMarker(latlng);
+// 	  },
+// 		onEachFeature: onEachFeature,
+// 		style: style
+// 	});
+// 	allProjects.addTo(myMap);
+// });
+
+
 
