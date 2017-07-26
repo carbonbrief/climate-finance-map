5d5ae958aaab76be6bfa4dac9a549a8642e09f29
Rosamund <rosamund.pearce@carbonbrief.org>
2017-07-26 14:56:46 +0100

    attempt to add checkbox/remove tag filter

--- script.js
+++ script.js
@@ -65,26 +65,8 @@ function addClusterLayer (data) {
       }).on('click', function (e) {
         myMap.setView(e.latlng, 5);
       });
-
-      // add GCF only layer
-
-      var GCF = L.geoJson(data, {
-        filter: function (feature, layer) {
-          return (feature.properties.Fund === "GCF")
-        },
-        onEachFeature: onEachFeature,
-        style: style,
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
-              tag: ['GCF']
-            });
-        }
-      }).on('click', function (e) {
-        myMap.setView(e.latlng, 5);
-      });
       
       markers.addLayer(geoJsonLayer);
-      markers.addLayer(GCF);
       
       myMap.addLayer(markers);
       console.log("markers")			
@@ -137,13 +119,42 @@ function onEachFeature(feature, layer) {
     };
 }
 
-// control for filter button NB gcf layer currenlty not adding
+// try to get checkbox filter
 
-L.control.tagFilterButton({
-	data: ['GCF', 'Other'],
-	filterOnEveryClick: true,
-	openPopupOnHover: true
-}).addTo( myMap );
+var layers;
+
+L.featureLayer()
+    .loadURL('climatefinance.geojson')
+    .on('ready', function(e) {
+        layers = e.target;
+        showFunds();
+    });
+
+var filters = document.getElementById('Funds').filters;
+
+// There are many ways to filter data. Mapbox.js has the .setFilter method,
+// but it only applies to L.mapbox.featureLayer layers, and that isn't what
+// we're creating - we're making marker groups in a MarkerClusterGroup layer.
+// Thus we distill filtering down to its essential part: an 'if' statement
+// in a loop.
+function showFunds() {
+    // first collect all of the checked boxes and create an array of strings
+    // like ['green', 'blue']
+    var list = [];
+    for (var i = 0; i < filters.length; i++) {
+        if (filters[i].checked) list.push(filters[i].value);
+    }
+    // then remove any previously-displayed marker groups
+    overlays.clearLayers();
+    // create a new marker group
+    var clusterGroup = new L.MarkerClusterGroup().addTo(overlays);
+    // and add any markers that fit the filtered criteria to that group.
+    layers.eachLayer(function(layer) {
+        if (list.indexOf(layer.feature.properties.line) !== -1) {
+            clusterGroup.addLayer(layer);
+        }
+    });
+}
 
 
 // make key visibility responsive
