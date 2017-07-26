5c1b4ab2de2cb6cb56ed6d8d681a56aa6255d222
Rosamund <rosamund.pearce@carbonbrief.org>
2017-07-26 15:14:30 +0100

    Get rid of form

--- script.js
+++ script.js
@@ -119,43 +119,6 @@ function onEachFeature(feature, layer) {
     };
 }
 
-// try to get checkbox filter
-
-var layers;
-
-L.featureLayer()
-    .loadURL('climatefinance.geojson')
-    .on('ready', function(e) {
-        layers = e.target;
-        showFunds();
-    });
-
-var filters = document.getElementById('Funds').filters;
-
-// There are many ways to filter data. Mapbox.js has the .setFilter method,
-// but it only applies to L.mapbox.featureLayer layers, and that isn't what
-// we're creating - we're making marker groups in a MarkerClusterGroup layer.
-// Thus we distill filtering down to its essential part: an 'if' statement
-// in a loop.
-function showFunds() {
-    // first collect all of the checked boxes and create an array of strings
-    // like ['green', 'blue']
-    var list = [];
-    for (var i = 0; i < filters.length; i++) {
-        if (filters[i].checked) list.push(filters[i].value);
-    }
-    // then remove any previously-displayed marker groups
-    overlays.clearLayers();
-    // create a new marker group
-    var clusterGroup = new L.MarkerClusterGroup().addTo(overlays);
-    // and add any markers that fit the filtered criteria to that group.
-    layers.eachLayer(function(layer) {
-        if (list.indexOf(layer.feature.properties.line) !== -1) {
-            clusterGroup.addLayer(layer);
-        }
-    });
-}
-
 
 // make key visibility responsive
 
